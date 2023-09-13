5ef2043153e515233c5f56b012818938b9429dce
Vortex <vladimir.vortex@gmail.com>
2023-09-13 12:57:11 +0300

    fix create bot link

--- apps/nextjs/src/app/dashboard/my-bots/page.tsx
+++ apps/nextjs/src/app/dashboard/my-bots/page.tsx
@@ -29,7 +29,7 @@ export default async function BotPage() {
               </div>
               <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                 <Link
-                  href="/dashboard/my-chats/create"
+                  href="/dashboard/my-bots/create"
                   className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Add bot
