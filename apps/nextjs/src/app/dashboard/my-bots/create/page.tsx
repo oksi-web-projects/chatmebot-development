7f828f41ba9e5ebc1b5f9dc1d1e102e5b2d04860
Vortex <vladimir.vortex@gmail.com>
2023-09-19 14:04:07 +0300

    add nav link to chats and bots lists from create page

--- apps/nextjs/src/app/dashboard/my-bots/create/page.tsx
+++ apps/nextjs/src/app/dashboard/my-bots/create/page.tsx
@@ -5,6 +5,8 @@ import { auth } from "@chatmebot/auth";
 
 import "react-toastify/dist/ReactToastify.css";
 
+import Link from "next/link";
+
 import { api } from "~/utils/api";
 import { Container } from "~/components/Container";
 import MyBotCreateForm from "~/components/MyBotCreateForm";
@@ -19,8 +21,17 @@ export default async function BotCreatePage() {
     <>
       <Container className="pb-32 pt-8 text-center">
         <ToastContainer className="mx-auto max-w-2xl" position="top-center" />
+
         <div className="mx-auto max-w-2xl">
-          <div className="space-y-12 sm:space-y-16">
+          <div className="text-left">
+            <Link
+              className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              href="/dashboard/my-bots"
+            >
+              Back
+            </Link>
+          </div>
+          <div className="mt-8 space-y-12 sm:space-y-16">
             <div>
               <h1 className="text-lg font-semibold leading-7 text-gray-900">
                 Create bot
